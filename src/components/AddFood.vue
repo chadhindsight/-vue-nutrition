0d4c537872019ca8d4ad5fc8522faddfaae949e6
Chad Hinds <chadrickj8@gmail.com>
2020-09-01 12:45:54 -0400

    Setup component that adds new food item

---
+++ src/components/AddFood.vue
@@ -0,0 +1,45 @@
+<template>
+  <div>
+    <form @submit.prevent="addTodo">
+      <input type="text" v-model="title" name="title" placeholder="Add Food..." />
+      <input type="submit" value="Submit" class="btn" />
+    </form>
+  </div>
+</template>
+
+<script>
+// import uuid from 'uuid';
+export default {
+  name: "AddFood",
+  data() {
+    return {
+      name: "",
+      calories: 0,
+    };
+  },
+  methods: {
+    addTodo() {
+      const newFood = {
+        name: this.name,
+        calories: this.calories,
+      };
+      // Send up to parent
+      this.$emit("add-food", newFood);
+      this.title = "";
+    },
+  },
+};
+</script>
+
+<style scoped>
+form {
+  display: flex;
+}
+input[type="text"] {
+  flex: 10;
+  padding: 5px;
+}
+input[type="submit"] {
+  flex: 2;
+}
+</style>
